9781364215a91dbf6746ff61812f26f0cc310b74
trevinshu <trevinshu2008@gmail.com>
2020-11-27 14:25:11 -0700

    Finished Product.cs and added Registration.cs

--- HTPSSystem.TShu/Entities/Product.cs
+++ HTPSSystem.TShu/Entities/Product.cs
@@ -3,10 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+#region Additional Namespaces
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+#endregion
 namespace HTPSSystem.TShu.Entities
 {
-    class Product
+    [Table("Product")]
+    public class Product
     {
+        [Key]
+        public int ProductID { get; set; }
+        public string Name { get; set; }
+        [Required(ErrorMessage ="Name is required")]
+        [StringLength(50, ErrorMessage = "Name is limited to 50 characters")]
+        public string ModelNumber { get; set; }
+        [Required(ErrorMessage = "Model Number is required")]
+        [StringLength(50, ErrorMessage = "Model Number is limited to 15 characters")]
+        public Boolean Discontinued 
+        {  
+            set
+            {
+                Discontinued = true;
+            }
+        }
+        public DateTime DiscontinuedDate 
+        {  
+            set
+            {
+                DiscontinuedDate = DateTime.Now;
+            }
+        }
     }
 }
