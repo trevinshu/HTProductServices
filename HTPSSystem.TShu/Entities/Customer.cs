763b7269434cdfc18aa5a3b5d6bbe045dc72e17f
trevinshu <trevinshu2008@gmail.com>
2020-11-27 13:53:49 -0700

    Finished Customer.cs and added Product.cs

--- HTPSSystem.TShu/Entities/Customer.cs
+++ HTPSSystem.TShu/Entities/Customer.cs
@@ -1,12 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
+
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+#region Additional Namespaces
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+#endregion
 
 namespace HTPSSystem.TShu.Entities
 {
+    [Table("Customer")]
     public class Customer
     {
+        private string _ContactNumber;
+        [Key]
+        public int CustomerID { get; set; }
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "First Name is required.")]
+        [StringLength(50, ErrorMessage = "First Name is limited to 50 characters.")]
+        public string LastName { get; set; }
+        [Required(ErrorMessage = "Last Name is required.")]
+        [StringLength(50, ErrorMessage = "Last Name is limited to 50 characters.")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Email is required.")]
+        [StringLength(100, ErrorMessage = "Email is limited to 50 characters.")]
+        public string ContactNumber
+        {
+            get
+            {
+                return _ContactNumber;
+
+            }
+
+            set
+            {
+                _ContactNumber = string.IsNullOrEmpty(value) ? null : value;
+                
+            }
+        }
+
+        [NotMapped]
+        public string FullName
+        {
+            get
+            {
+                return LastName + "," + FirstName;
+            }
+        }
     }
 }
